395d8f35d0adf5b7912b5f32b961c67e84d9dffa
juanbuiles67227 <31153055+juanbuiles67227@users.noreply.github.com>
2018-10-23 02:07:20 -0500

    Area y Radio

---
+++ Tarea2ASM/asm.asm
@@ -0,0 +1,32 @@
+.model flat, C
+.data
+
+	pi DD 3
+	dos DD 2
+	
+.code
+Area proc
+	push ebp
+	mov ebp, esp
+
+	mov eax, [ebp+8]
+	cdq
+	idiv dos
+	
+	mov edx,eax
+	imul eax, edx 
+	imul eax, pi 
+	mov [ebp+12], edx
+	pop ebp
+	ret 
+Area endp
+
+Radio proc
+	push ebp
+	mov ebp, esp
+	pop ebp
+	ret 
+Radio endp
+
+
+end
